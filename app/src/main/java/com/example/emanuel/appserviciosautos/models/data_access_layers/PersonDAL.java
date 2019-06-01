110eab4b43ec04a276d3b9ee19fdfff90075056a
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-01 15:41:22 -0600

    Creada Vista para dar de baja

--- app/src/main/java/com/example/emanuel/appserviciosautos/models/data_access_layers/PersonDAL.java
+++ app/src/main/java/com/example/emanuel/appserviciosautos/models/data_access_layers/PersonDAL.java
@@ -1,4 +1,7 @@
 package com.example.emanuel.appserviciosautos.models.data_access_layers;
 
+import com.example.emanuel.appserviciosautos.db_conection.SQLiteConnection;
+
 public class PersonDAL {
+    SQLiteConnection connection;
 }
