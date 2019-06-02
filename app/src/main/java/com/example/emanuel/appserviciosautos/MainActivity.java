ea3a7b27fb24048618b05670d23a4d28dd3330b3
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-02 12:52:09 -0600

    Añadida funcionalidad a botones en actividad principal

--- app/src/main/java/com/example/emanuel/appserviciosautos/MainActivity.java
+++ app/src/main/java/com/example/emanuel/appserviciosautos/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.emanuel.appserviciosautos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnPerson,btnCar,btnService,btnQuerry;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +22,31 @@ public class MainActivity extends AppCompatActivity {
         this.btnQuerry = findViewById(R.id.btnQuerry);
     }
 
-    public Button getBtnPerson() {
-        return btnPerson;
+    @Override
+    public void onClick(View view) {
+
+        if(view == this.btnPerson){
+            openSelectActivity("person",0);
+            return;
+        }
+        if(view == this.btnCar){
+            openSelectActivity("car",1);
+            return;
+        }
+        if(view == this.btnService){
+            openSelectActivity("service",2);
+            return;
+        }
+        if(view == this.btnQuerry){
+            openSelectActivity("querry",3);
+            return;
+        }
+
     }
 
-    public Button getBtnCar() {
-        return btnCar;
-    }
-
-    public Button getBtnService() {
-        return btnService;
-    }
-
-    public Button getBtnQuerry() {
-        return btnQuerry;
+    private void openSelectActivity(String key, int value){
+        intent = new Intent(this, ConsultActivity.class);
+        intent.putExtra(key,value);
+        startActivity(intent);
     }
 }
