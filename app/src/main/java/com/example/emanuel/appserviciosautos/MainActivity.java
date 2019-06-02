59018c541029239b715edf82125f8d55cc72f4bf
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-02 12:43:54 -0600

    Añadidos botones a la actividad principal

--- app/src/main/java/com/example/emanuel/appserviciosautos/MainActivity.java
+++ app/src/main/java/com/example/emanuel/appserviciosautos/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.emanuel.appserviciosautos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPerson,btnCar,btnService,btnQuerry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnPerson = findViewById(R.id.btnPerson);
+        this.btnCar = findViewById(R.id.btnCar);
+        this.btnService = findViewById(R.id.btnService);
+        this.btnQuerry = findViewById(R.id.btnQuerry);
+    }
+
+    public Button getBtnPerson() {
+        return btnPerson;
+    }
+
+    public Button getBtnCar() {
+        return btnCar;
+    }
+
+    public Button getBtnService() {
+        return btnService;
+    }
+
+    public Button getBtnQuerry() {
+        return btnQuerry;
     }
 }
