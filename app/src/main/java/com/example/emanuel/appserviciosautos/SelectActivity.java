8b4b280892ace672c7ceab9682a5235fccb9a60b
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-02 15:30:19 -0600

    Añadida funcionaidad inicial a actividad de selección

--- app/src/main/java/com/example/emanuel/appserviciosautos/SelectActivity.java
+++ app/src/main/java/com/example/emanuel/appserviciosautos/SelectActivity.java
@@ -1,12 +1,18 @@
 package com.example.emanuel.appserviciosautos;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class SelectActivity extends AppCompatActivity {
+public class SelectActivity extends AppCompatActivity implements View.OnClickListener{
 
     int type;
+    ImageView imgLogo;
+    Button btnInsert, btnRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,34 @@ public class SelectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select);
 
         Intent intent = getIntent();
-        type = intent.getIntExtra("type");
+        type = intent.getIntExtra("type",0);
+        this.btnInsert = findViewById(R.id.btnInsert);
+        this.btnRemove = findViewById(R.id.btnRemove);
+        imgLogo= findViewById(R.id.imgLogo);
+        if(type == 1)
+            imgLogo.setImageResource(R.drawable.car_icon);
+        if(type == 2)
+            imgLogo.setImageResource(R.drawable.services_icon);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        if(view == btnInsert){
+            if(type == 0)
+                intent = new Intent(this,RegistryActivity.class);
+            if(type == 1)
+                intent = new Intent(this,RegistryActivity.class);
+            if(type == 2)
+                intent = new Intent(this,RegistryActivity.class);
+
+            return;
+        }
+        if(view == btnRemove){
+            intent = new Intent(this,RemoveActivity.class);
+            intent.putExtra("type",type);
+            return;
+        }
     }
 }
