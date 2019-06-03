6b0ff986940d8940923448af964e7703c96e9389
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-03 15:13:16 -0600

    Agregadas expresiones regulares para Personas y Carros

--- app/src/main/java/com/example/emanuel/appserviciosautos/utils/RegexConstants.java
+++ app/src/main/java/com/example/emanuel/appserviciosautos/utils/RegexConstants.java
@@ -2,13 +2,15 @@ package com.example.emanuel.appserviciosautos.utils;
 
 public class RegexConstants {
 
-    public final static String RFC_REGEX ="";
-    public final static String NAME_REGEX ="";
-    public final static String CITY_REGEX ="";
+    public final static String RFC_REGEX = "[A-Za-z]{4}[0-9]{9}";
+    public final static String NAME_REGEX = "[A-Za-z]{10,30}";
+    public final static String CITY_REGEX = "[A-Za-z]{4,20}";
 
-    public final static String PLATE_REGEX = "";
-    public final static String TRADEMARK_REGEX = "";
-    public final static String MODEL_REGEX = "";
-    public final static String YEAR_REGEX = "";
+    public final static String PLATE_REGEX = "[A-Za-z]{3}[0-9]{4}";
+    public final static String TRADEMARK_REGEX = "[A-Za-z]{3,14}";
+    public final static String MODEL_REGEX = "[A-Za-z]{3,14}";
+    public final static String YEAR_REGEX = "[1-2]{1}[0-9]{3}";
+
+    public final static String DATE_REGEX = "";
 
 }
