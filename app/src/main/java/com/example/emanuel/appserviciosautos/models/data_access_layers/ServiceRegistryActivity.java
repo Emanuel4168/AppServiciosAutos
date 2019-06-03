bfaf446187236e27f9da080bb7cef2c1778cdb1c
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-03 09:19:14 -0600

    Cmbios

---
+++ app/src/main/java/com/example/emanuel/appserviciosautos/models/data_access_layers/ServiceRegistryActivity.java
@@ -0,0 +1,13 @@
+package com.example.emanuel.appserviciosautos.models.data_access_layers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceRegistryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_registry);
+    }
+}
