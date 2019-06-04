d8c7241dcc4fea809b4c19a6f88cb8704eae911a
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-04 15:19:31 -0600

    Creada clase Routines con métodos auxiliares

---
+++ app/src/main/java/com/example/emanuel/appserviciosautos/utils/Routines.java
@@ -0,0 +1,14 @@
+package com.example.emanuel.appserviciosautos.utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class Routines {
+
+    public void showNotificationMessage(Context context, String title, String mesage){
+        AlertDialog Alerta = new AlertDialog.Builder(context).create();
+        Alerta.setTitle(title);
+        Alerta.setMessage(mesage);
+        Alerta.show();
+    }
+}
