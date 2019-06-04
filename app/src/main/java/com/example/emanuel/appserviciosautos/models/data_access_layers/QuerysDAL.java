d287d9d383dd2c43795eabed79fdd82526f8c419
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-04 01:03:27 -0600

    Corregida funcionalidad de QuerrysDAL

--- app/src/main/java/com/example/emanuel/appserviciosautos/models/data_access_layers/QuerysDAL.java
+++ app/src/main/java/com/example/emanuel/appserviciosautos/models/data_access_layers/QuerysDAL.java
@@ -1,11 +1,15 @@
 package com.example.emanuel.appserviciosautos.models.data_access_layers;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.emanuel.appserviciosautos.db_conection.SQLiteConnection;
 import com.example.emanuel.appserviciosautos.utils.DataBaseConstants;
+import com.example.emanuel.appserviciosautos.utils.QueryItem;
+
+import java.util.LinkedList;
 
 public class QuerysDAL {
 
@@ -14,42 +18,61 @@ public class QuerysDAL {
     private Context context;
     private String error, errorDefault;
 
-    public void revenueByCity(){
+    public LinkedList<QueryItem> revenueByCity(){
         dataBase = connection.getWritableDatabase();
-        String query = "Select sum("+ DataBaseConstants.SERVICE_PRICE+") from "+DataBaseConstants.SERVICES_TABLE+" S"
-                +" inner join "+DataBaseConstants.PERSONS_TABLE+" P on S."+DataBaseConstants.PERSONS_RFC+" = P."+DataBaseConstants.PERSONS_RFC
-                +" group by P."+DataBaseConstants.PERSONS_CITY;
+        LinkedList<QueryItem> res = new LinkedList<QueryItem>();
+        String query = "select sum(price) as total, min(price) as min, max(price) as max, avg(price) as avg from SERVICES S inner join PERSONS P on S.rfc = P.rfc group by P.city";
         try{
-            dataBase.execSQL(query);
+            Cursor c = dataBase.rawQuery(query,null);
+            while(c.moveToNext()){
+                String[] fields = {c.getFloat(0)+"",c.getFloat(1)+"",c.getFloat(2)+"",c.getFloat(3)+""};
+                res.add(new QueryItem(fields));
+            }
+            return res;
         }catch (Exception e){
-
+            error = "";
+            return null;
         }finally {
             dataBase.close();
         }
     }
 
-    public void servicesYearCityTrademark(){
+    public LinkedList<QueryItem> servicesYearCityTrademark(){
         dataBase = connection.getWritableDatabase();
-        String query = "Select sum("+ DataBaseConstants.SERVICE_PRICE+") from "+DataBaseConstants.PERSONS_TABLE+" P"
-                +" left join "+DataBaseConstants.SERVICES_TABLE+" S on P."+DataBaseConstants.PERSONS_RFC+" is null";
+        LinkedList<QueryItem> res = new LinkedList<QueryItem>();
+        String query = "select PERSONS.rfc,name,city from PERSONS P left join SERVICES S on(P.rfc = S.rfc) where P.rfc is null";
         try{
-            dataBase.execSQL(query);
-        }catch (Exception e){
+            Cursor c = dataBase.rawQuery(query,null);
+            while(c.moveToNext()){
+                String[] fields = {c.getString(0),c.getString(1),c.getString(2),""};
+                res.add(new QueryItem(fields));
+            }
+            return res;
 
+        }catch (Exception e){
+            error = "";
+            return null;
         }finally {
             dataBase.close();
         }
     }
 
-    public void personsWithoutService(){
+    public LinkedList<QueryItem> personsWithoutService(){
         dataBase = connection.getWritableDatabase();
-        String query = "Select substr(date,1,4) as year, city, trademark, count(*) as nServices, sum(price) as total" +
+        LinkedList<QueryItem> res = new LinkedList<QueryItem>();
+        String query = "Select substr(date,1,4) as year, city, trademark, count(*) as nServices" +
                         " from SERVICES S inner join PERSONS P on (S.rfc = P.rfc) inner join CARS C on (S.plate = C.plate)" +
                         " group by year, city, trademark";
         try{
-            dataBase.execSQL(query);
+            Cursor c = dataBase.rawQuery(query,null);
+            while(c.moveToNext()){
+                String[] fields = {c.getString(0),c.getString(1),c.getString(1),c.getInt(2)+""};
+                res.add(new QueryItem(fields));
+            }
+            return res;
         }catch (Exception e){
-
+            error = "";
+            return null;
         }finally {
             dataBase.close();
         }
